50d3d82624cea8a1e050b95f4d3f2c03dd3057b7
Jan Ptacek <jan.ptacek@gmail.com>
2015-06-03 21:00:42 +0200

    routes

--- Config/routes.php
+++ Config/routes.php
@@ -16,7 +16,7 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+	Router::connect('/', array('controller' => 'bookings', 'action' => 'index', 'admin'=>true));
 	Router::connect('/login', array('controller' => 'users', 'action' => 'login'));
 	Router::connect('/logout', array('controller' => 'users', 'action' => 'logout'));
 
